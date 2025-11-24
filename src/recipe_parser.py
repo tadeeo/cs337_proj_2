68c17893692cb2003e9c650d58dc71eb211e6f86
tadeeo <tadeo@u.northwestern.edu>
2025-11-24 13:15:41 -0600

    Revert "scraped tools, ing_subs, modfySt3, debugging handler"
    This reverts commit db2fda0ad21fe9fedd84e2e0d0d2860823f52ad3.

--- src/recipe_parser.py
+++ src/recipe_parser.py
@@ -1,42 +1,50 @@
 import json
-from parser_1 import load_list_from_file, parse_step
+from parser_1 import load_list_from_file, parse_step_main
 
 def load_tools():
-    tools_file = 'src/tools.txt'
+    tools_file = 'tools.txt'
     tools = load_list_from_file(tools_file)
     return tools
 
 def load_ingredients():
     with open("recipe.json", "r") as f:
         data = json.load(f)
-    return data["ingredients"]   # Return list of dicts {qty, unit, name}
+
+    ingredients = [item["name"] for item in data["ingredients"]]
+    return ingredients
 
 def load_steps():
     with open("recipe.json", "r") as f:
         data = json.load(f)
-    steps = []
-    for step in data["steps"]:
-        for sub in step["substeps"]:
-            steps.append({ "step_number": step["step_number"], "text": sub["text"] })
-    return steps
+
+    text = []
+    #sub_steps = [{"substeps": item["substeps"], "step_number": step for item in data["steps"]]
+    for sub in data["steps"]:
+        for i in range(len(sub["substeps"])):
+            text.append({ "step_number": sub["step_number"], "substep_number": sub["substeps"][i]["sub_number"], "text": sub["substeps"][i]["text"] })
+    
+    print(text)        
+    return text
 
 def get_parsed_steps():
     steps = load_steps()
     tools = load_tools()
-    ingredient_data = load_ingredients()
+    ingredients = load_ingredients()
     parsed_steps = []
-    step_counter = 1
+
     for step in steps:
-        parsed_step = parse_step(step_counter, step['text'], ingredient_data, tools)
-        parsed_step["step_number"] = step_counter
+        parsed_step = parse_step_main(step['text'], tools, ingredients)
+        parsed_step["step_number"] = step["step_number"]
+        parsed_step["substep_number"] = step["substep_number"]
+        parsed_step = parse_step_main(step['text'], tools, ingredients)
         parsed_steps.append(parsed_step)
-        step_counter += 1
     return parsed_steps
 
 def main():
     data = get_parsed_steps()
     with open("parsed_recipes.json", "w") as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
+
 
 if __name__ == "__main__":
     main()
